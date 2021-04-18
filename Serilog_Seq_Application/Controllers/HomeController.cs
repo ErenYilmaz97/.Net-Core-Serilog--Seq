7f429cfe0e47f46a3ad3b0dfbf984e5d670dbe7e
Eren YILMAZ <erenyilmazgazi@gmail.com>
2021-04-18 12:41:10 +0300

    init

---
+++ Serilog_Seq_Application/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Serilog_Seq_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //DI
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        public void Index()
+        {
+            string adSoyad = "Eren Yılmaz";
+            var complexType = new {Name = "Eren", Lastname = "Yılmaz", Age = 23, Gender = "Man"};
+
+            _logger.LogInformation("Home Index Triggered.");
+            _logger.LogInformation("{adSoyad} Kullanıcısı Sisteme Giriş Yaptı.", adSoyad);
+            _logger.LogInformation("{@complexType} Sisteme Giriş Yaptı", complexType);  //COMPLEX TYPE'LAR SERIALIZE EDİLİR.
+
+        }
+
+
+
+
+    }
+}
---
+++ Serilog_Seq_Application/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog_Seq_Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger  = new LoggerConfiguration()
+                .WriteTo.Debug(Serilog.Events.LogEventLevel.Information)
+                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day) //HER GÜN ÝÇÝN YENÝ DOSYA OLUÞTUR
+                .WriteTo.Seq("http://localhost:5341/") //LOG SERVER URL
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                .MinimumLevel.Override("System", LogEventLevel.Error)
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .ConfigureLogging(config => config.ClearProviders())
+                .UseSerilog();
+    }
+}
+
